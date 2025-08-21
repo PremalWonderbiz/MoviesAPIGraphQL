664df8c4502f144fb9bc29b91ad961d4139abee5
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-08-21 18:18:08 +0530

    first commit

---
+++ Data/MoviesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movies.API.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public DbSet<Models.Movie> Movies { get; set; }
+        public DbSet<Models.MovieReview> MovieReviews { get; set; }
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Movie>().Property(m => m.Genre).HasConversion<string>();                
+        }
+    }
+}
